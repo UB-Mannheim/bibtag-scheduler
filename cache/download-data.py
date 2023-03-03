UB-Mannheim/bibtag-scheduler

commit ffb430a8bf8d778f883a5d33dcea79b8808c872d

Stabilize path for output files in download-data.py
Before the script needed to be called from within the cache directory
to save all the data there as well. With this change one can also call
the script from the main directory.

cache/download-data.py
@@ -34,8 +34,12 @@ presentationUrl = apiUrl + '/program/presentations/'
 # Options:
 #    https://bid2022.planner.documedias.systems/api/program/options
 
+
+# reliably open a file in the same directory as the current script
+p = Path(__file__)
+
 # download main data
-with open('index.json', 'w') as outfile:
+with p.with_name('index.json').open('w') as outfile:
     data = []
     for day in range(1, numberOfDays + 1):
         req = requests.get(apiUrl + "/program/days/" + str(day), verify=False)
@@ -53,13 +57,13 @@ for session in data:
     data = req.json()
     if len(data) > 1:
         print("Unexpected format for session" + sessionId)
-    with open('s' + sessionId + '.json', 'w') as outfile:
+    with p.with_name('s' + sessionId + '.json').open('w') as outfile:
         json.dump(data, outfile, indent=4)
     if 'presentations' in data[0]:
         for presentation in data[0]['presentations']:
             presentationIds.append(presentation["presentation"]["id"])
 
-with open('presentationtIds', 'w') as outfile:
+with p.with_name('presentationtIds', 'w').open('w') as outfile:
     json.dump(presentationIds, outfile, indent=4)
 
 # download details of all presentations
@@ -85,5 +89,5 @@ for presentationId in presentationIds:
                 data[0]["abstract_enriched"] = abstract
         else:
             print(req.status_code, "Error when requesting the abstract", abstractUrl, "for presentation", presentationId, data[0]["title"])
-    with open('p' + str(presentationId) + '.json', 'w') as outfile:
+    with p.with_name('p' + str(presentationId) + '.json', 'w').open('w') as outfile:
         json.dump(data, outfile, indent=4)

commit b453714ec5e55aff659324c8e1e70f459ce57e71

🐛 Fix two Path.pathlib.with_name calls
This fixes a regression from the commit
ffb430a8bf8d778f883a5d33dcea79b8808c872d

cache/download-data.py
@@ -63,7 +63,7 @@ for session in data:
         for presentation in data[0]['presentations']:
             presentationIds.append(presentation["presentation"]["id"])
 
-with p.with_name('presentationtIds', 'w').open('w') as outfile:
+with p.with_name('presentationtIds').open('w') as outfile:
     json.dump(presentationIds, outfile, indent=4)
 
 # download details of all presentations
@@ -89,5 +89,5 @@ for presentationId in presentationIds:
                 data[0]["abstract_enriched"] = abstract
         else:
             print(req.status_code, "Error when requesting the abstract", abstractUrl, "for presentation", presentationId, data[0]["title"])
-    with p.with_name('p' + str(presentationId) + '.json', 'w').open('w') as outfile:
+    with p.with_name('p' + str(presentationId) + '.json').open('w') as outfile:
         json.dump(data, outfile, indent=4)